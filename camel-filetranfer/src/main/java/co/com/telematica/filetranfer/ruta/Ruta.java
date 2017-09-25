1a3a97d53d74c01e581615d10ca99690f1278977
jnarino <jnarino@summan.com>
2017-09-25 02:31:57 -0500

    ruta funcional insertando registros de movimiento de datos

--- camel-filetranfer/src/main/java/co/com/telematica/filetranfer/ruta/Ruta.java
+++ camel-filetranfer/src/main/java/co/com/telematica/filetranfer/ruta/Ruta.java
@@ -1,13 +1,38 @@
 package co.com.telematica.filetranfer.ruta;
 
+import java.util.HashMap;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 public class Ruta extends RouteBuilder {
 
+	public int i = 4;
+
 	@Override
 	public void configure() throws Exception {
-		// TODO Auto-generated method stub
-		from("timer:test?repeatCount=3").setBody(simple("hola")).log(" Esta recibiendo  ${body}").end();
+
+		from("file:{{directorio.in}}?noop=true&move=.done").process(new Processor() {
+
+			@Override
+			public void process(Exchange exchange) throws Exception {
+
+				System.out.println("Entra a tomar nombres");
+
+				String fileName = exchange.getIn().getHeader("CamelFileName").toString();
+				String fileLength = exchange.getIn().getHeader("CamelFileLength").toString();
+				String fileModified = exchange.getIn().getHeader("CamelFileLastModified").toString();
+				System.out.println(fileName + "-" + fileLength + "-" + fileModified);
+				exchange.getIn().setHeader("ids", i);
+				i += 1;
+
+			}
+		}).log("El archivo con nombre ${header.CamelFileName} ha sido tranferido").to("sqlPostgres:{{insert.register}}")
+				.log("${body}").to("file:{{directorio.out}}").end();
+
+		// from("timer:pepito?repeatCount=1")
 	}
 
 }
